9dd03f98a7737d75b3f59f95d5585c3042d76c78
Miles <milesgregg3@gmail.com>
2021-06-05 23:48:57 -0600

    tests

--- python/Testing.py
+++ python/Testing.py
@@ -2,15 +2,21 @@ import cv2
 
 cap = cv2.VideoCapture("data/data.mp4")
 
+modelTest = cv2.CascadeClassifier('cars.xml')
+
 while cap.isOpened():
     ret, frame = cap.read()
 
-    if ret:
-        cv2.imshow('Frame', frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
-    else:
+    cars = modelTest.detectMultiScale(gray, 1.1, 1)
+
+    for (x, y, w, h) in cars:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('Frame', frame)
+
+    if cv2.waitKey(33) == 27:
         break
 
 cap.release()
