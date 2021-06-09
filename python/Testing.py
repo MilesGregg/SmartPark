e01227c64befb115bf31b709a71f1266db70b7c9
Miles <milesgregg3@gmail.com>
2021-06-09 02:51:55 -0600

    cars.xml didn't work.......

--- python/Testing.py
+++ python/Testing.py
@@ -2,17 +2,9 @@ import cv2
 
 cap = cv2.VideoCapture("data/data.mp4")
 
-modelTest = cv2.CascadeClassifier('cars.xml')
 
 while cap.isOpened():
     ret, frame = cap.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cars = modelTest.detectMultiScale(gray, 1.1, 1)
-
-    for (x, y, w, h) in cars:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     cv2.imshow('Frame', frame)
 
