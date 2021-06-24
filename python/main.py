882a28cf411085d146916c802661499eb4731c95
Miles <milesgregg3@gmail.com>
2021-06-24 02:32:38 -0600

    test flask api

--- python/main.py
+++ python/main.py
@@ -1,10 +1,16 @@
 from flask import Flask
-app = Flask(__name__)
+import time
 
+
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
 
 if __name__ == '__main__':
     app.run(host="localhost", port=3000)
